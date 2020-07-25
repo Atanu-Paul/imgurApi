0b384ab3ed81235bb439b536b095ef1488150e68
Atanu Paul <38816880+Atanu-Paul-au6@users.noreply.github.com>
2020-07-26 00:54:40 +0530

    adding cors-1

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const path = require("path");
 const photosRouter = require("./routers/photos");
 const connectDB = require("./db/connection");
 
-const cors = require('cors')
-const corsOptions = {
+const cors = require('cors');
+
+//app.use(cors());
+
+app.use(cors({
   origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+  credentials: true
+}));
 
 const app = express();
 const PORT = process.env.PORT || 3300;
