6e05f883cc4693fe957ac37a1aad20426ca57a1a
Atanu Paul <38816880+Atanu-Paul-au6@users.noreply.github.com>
2020-07-26 01:13:29 +0530

    modifing cors-2

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const cors = require('cors');
 
 app.use(cors());
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const app = express();
 const PORT = process.env.PORT || 3300;
 
