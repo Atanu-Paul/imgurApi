97006b5992dd58081873f444258a1081342dbb6a
Atanu Paul <38816880+Atanu-Paul-au6@users.noreply.github.com>
2020-07-26 01:26:55 +0530

    adding cors-3

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const PORT = process.env.PORT || 3300;
 
 //call to DB
 connectDB();
+
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
